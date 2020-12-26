ceda20737addaf9b3bf497ecf107f1491c9f17cc
Jadir Junior <jadirjsjunior@gmail.com>
2020-12-26 15:24:11 -0300

    material card / card header and content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,25 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+    <div>
+      <mat-toolbar color="primary"><span>LocalCast Weather</span></mat-toolbar>
+      <div>Yout city, your forecast, right now!</div>
+      <div fxLayout="row">
+        <div fxFlex></div>
+        <div fxFlex="300px">
+          <mat-card>
+            <mat-card-header>
+              <mat-card-title>Current Weather</mat-card-title>
+            </mat-card-header>
+            <mat-card-content>
+              <app-current-weather></app-current-weather>
+            </mat-card-content>
+          </mat-card>
+        </div>
+        <div fxFlex></div>
+      </div>
+    </div>
+  `,
 })
 export class AppComponent {}
