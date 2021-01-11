8f790aad8c7d9dd5e1911a0defe67654b8e03165
Jadir Junior <jadirjsjunior@gmail.com>
2021-01-11 10:07:50 -0300

    aligning elements with CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,19 @@ import { Component } from '@angular/core'
   template: `
     <div>
       <mat-toolbar color="primary"><span>LocalCast Weather</span></mat-toolbar>
-      <div>Yout city, your forecast, right now!</div>
+      <div fxLayoutAlign="center">
+        <div class="mat-caption vertical-margin">
+          Your city, your forecast, right now!
+        </div>
+      </div>
       <div fxLayout="row">
         <div fxFlex></div>
         <div fxFlex="300px">
           <mat-card>
             <mat-card-header>
-              <mat-card-title>Current Weather</mat-card-title>
+              <mat-card-title>
+                <div class="mat-headline">Current Weather</div>
+              </mat-card-title>
             </mat-card-header>
             <mat-card-content>
               <app-current-weather></app-current-weather>
